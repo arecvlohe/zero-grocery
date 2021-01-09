0f0064923446377e4261491f361535a62927e930
Adam Recvlohe <adam.recvlohe@pm.me>
2021-01-09 16:08:48 -0600

    Update store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,9 +3,11 @@ import { combineEpics } from "redux-observable";
 
 import { groceriesReducer } from "./reducer.groceries";
 import { groceriesEpic } from "./epics.groceries";
+import { cartReducer } from "./reducer.cart";
 
 export const rootEpic = combineEpics(groceriesEpic);
 
 export const rootReducer = combineReducers({
   groceriesReducer,
+  cartReducer,
 });

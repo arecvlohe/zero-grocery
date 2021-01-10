3cc7b45b51f09e53c6cc7f1254ad5583aab48c4a
Adam Recvlohe <adam.recvlohe@pm.me>
2021-01-09 19:13:41 -0600

    Render basic UI with no styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as RD from "@devexperts/remote-data-ts";
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/pipeable";
+
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getGroceriesRequest } from "./redux/actions.groceries";
+import { groceriesSelector } from "./redux/selectors.groceries";
+import { Grocery } from "./redux/types.appState";
+import { addGroceryItem } from "./redux/actions.cart";
+import { cartSelector } from "./redux/selectors.cart";
 
 function App() {
+  const dispatch = useDispatch();
+  const groceriesRD = useSelector(groceriesSelector);
+  const cart = useSelector(cartSelector);
+
+  const addToCart = (grocery: Grocery) => {
+    dispatch(addGroceryItem(grocery));
+  };
+
+  React.useEffect(() => {
+    dispatch(getGroceriesRequest());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {pipe(
+        groceriesRD,
+        RD.fold(
+          () => <div>Initiating...</div>,
+          () => <div>Loading...</div>,
+          (_err) => (
+            <div>Sorry, there was an error fetching the resources.</div>
+          ),
+          (data) => {
+            const groceries = Object.values(data);
+
+            const items = groceries.map((grocery) => {
+              return pipe(
+                grocery,
+                RD.toOption,
+                O.fold(
+                  () => <div>There was an error fetching the grocery item</div>,
+                  (g) => {
+                    return (
+                      <div key={g.id}>
+                        {g.name}{" "}
+                        <button type="button" onClick={() => addToCart(g)}>
+                          Add to Cart
+                        </button>
+                      </div>
+                    );
+                  }
+                )
+              );
+            });
+
+            return <div>{items}</div>;
+          }
+        )
+      )}
+      <div>
+        Number of items in cart:{" "}
+        {Object.values(cart).reduce((acc, curr) => curr.count + acc, 0)}
+      </div>
     </div>
   );
 }
